25a709919d4ab28a310614c52cedfec75367e15c
FUSION 6763 <contact@fusion6763.org>
2023-02-20 17:55:02 -0500

    ostrich smells

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,8 +1,8 @@
 public class hello {
     public static void main(String[] args) {
         int num = 2;
-        for (int i = 0; i < 20; i++) {
-            System.out.println("ostrich smells");
+        for (int i = 0; i < Double.POSITIVE_INFINITY; i++) {
+            System.out.println("ostrich smells x" + i);
         }
     }
 }
